cc17264a66603c2a1e2bb4c478a955553667a62a
Junior Andrade <seisxis@gmail.com>
2023-03-08 15:01:59 -0300

    1

--- pytrustnfe/nfse/issnet/__init__.py
+++ pytrustnfe/nfse/issnet/__init__.py
@@ -65,7 +65,7 @@ def _send(certificado, method, **kwargs):
     transport = Transport(session=session)
 
     client = Client(wsdl='{}?wsdl'.format(base_url), transport=transport)
-    with client.settings(strict=False):
+    with client.settings(strict=False, raw_response=True):
         xml_send = {
             "nfseDadosMsg": kwargs["xml"],
             "nfseCabecMsg": """<?xml version="1.0"?>
@@ -85,7 +85,7 @@ def _send(certificado, method, **kwargs):
                 return client.service
 
         service = get_service(client=client, translation=('nfse.asmx', base_url))
-        response = service[method](**xml_send).__dict__
+        response = service[method](**xml_send)
 
         print ('--- response ---')
         print (response)
