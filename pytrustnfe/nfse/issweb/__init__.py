e3b264285f9b03bd6368a5d9855dfdfe992fa036
Christian David <christian@tsmx.net.br>
2023-07-07 15:09:34 -0300

    issweb 'security'

--- pytrustnfe/nfse/issweb/__init__.py
+++ pytrustnfe/nfse/issweb/__init__.py
@@ -21,31 +21,31 @@ import requests
 def _render_xml(certificado, method, **kwargs):
     kwargs['method'] = method
     path = os.path.join(os.path.dirname(__file__), "templates")
-    parser = etree.XMLParser(
-        remove_blank_text=True, remove_comments=True, strip_cdata=False
-    )
-    signer = Assinatura(certificado.pfx, certificado.password)
+    #parser = etree.XMLParser(
+        #remove_blank_text=True, remove_comments=True, strip_cdata=False
+    #)
+    #signer = Assinatura(certificado.pfx, certificado.password)
 
     #referencia = ""
     xml_string_send = render_xml(path, "%s.xml" % method, True, **kwargs)
     # xml object
-    xml_send = etree.fromstring(
-        xml_string_send, parser=parser)
+    #xml_send = etree.fromstring(
+        #xml_string_send, parser=parser)
 
-    if method == "recepcionarLoteRps":
+    #if method == "recepcionarLoteRps":
         #referencia = kwargs.get("nfse").get("numero_lote")
-        for item in kwargs["nfse"]["lista_rps"]:
-            reference = "rps:{0}{1}".format(
-                item.get('numero'), item.get('serie'))
+        #for item in kwargs["nfse"]["lista_rps"]:
+            #reference = "rps:{0}{1}".format(
+                #item.get('numero'), item.get('serie'))
             
-            signer.assina_xml(xml_send, reference)
+            #signer.assina_xml(xml_send, reference)
 
         #xml_signed_send = signer.assina_xml(
              #xml_send, "lote:{0}".format(referencia))
     #else:
         #xml_signed_send = etree.tostring(xml_send)
 
-    return etree.tostring(xml_send)
+    return xml_string_send
 
 def _send(certificado, method, **kwargs):
     if kwargs["ambiente"] == "homologacao":
