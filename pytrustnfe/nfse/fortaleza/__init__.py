9b5f291845d01983229a725ddfff44bc865d8a24
christian-tsmx <christian@tsmx.net.br>
2023-11-06 18:02:17 -0300

    #757 - 2

--- pytrustnfe/nfse/fortaleza/__init__.py
+++ pytrustnfe/nfse/fortaleza/__init__.py
@@ -3,18 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import os
-from OpenSSL import crypto
-from base64 import b64encode
 
 from requests import Session
-from zeep import Client
-from zeep.transports import Transport
-from requests.packages.urllib3 import disable_warnings
 
 from pytrustnfe.xml import render_xml, sanitize_response
 from pytrustnfe.certificado import extract_cert_and_key_from_pfx, save_cert_key
 from pytrustnfe.nfse.siasp.assinatura import Assinatura
 from lxml import etree
+import requests
 
 def _render(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
@@ -40,19 +36,32 @@ def _send(certificado, method, **kwargs):
     cert, key = extract_cert_and_key_from_pfx(certificado.pfx, certificado.password)
     cert, key = save_cert_key(cert, key)
 
-    header = '<ns2:cabecalho xmlns:ns2="http://www.ginfes.com.br/cabecalho_v03.xsd" versao="3"><versaoDados>3</versaoDados></ns2:cabecalho>'  # noqa
+    xml_send = kwargs["xml"]
+    op = method
+    if method == "RecepcionarLoteRpsV3":
+        op = "EnviarLoteRpsEnvio"
+    elif method == "ConsultarLoteRpsV3":
+        op = "ConsultarLoteRpsEnvio"
+    elif method == "CancelarNfse":
+        op = "CancelarNfseEnvio"
+    path = os.path.join(os.path.dirname(__file__), "templates")
+    soap = render_xml(path, "SoapRequest.xml", False, **{"soap_body":xml_send, "method": method, "op": op})
 
-    disable_warnings()
     session = Session()
     session.cert = (cert, key)
     session.verify = False
-    transport = Transport(session=session)
+    headers = {
+        "Content-Type": "text/xml;charset=UTF-8",
+        "Content-length": str(len(soap))
+    }
 
-    client = Client(wsdl=base_url, transport=transport)
+    request = requests.post(base_url, data=soap, headers=headers)
+    try:
+        response, obj = sanitize_response(request.content.decode('utf8', 'ignore'))
+    except Exception as e:
+        return {"sent_xml": str(soap), "received_xml": str(e), "object": None}
 
-    response = client.service[method](header,kwargs["xml"])
-    response, obj = sanitize_response(response)
-    return {"sent_xml": kwargs["xml"], "received_xml": response, "object": obj}
+    return {"sent_xml": str(soap), "received_xml": str(response.encode('utf8')), "object": obj.Body }
 
 def xml_recepcionar_lote_rps(certificado, **kwargs):
     return _render(certificado, "RecepcionarLoteRpsV3", **kwargs)
