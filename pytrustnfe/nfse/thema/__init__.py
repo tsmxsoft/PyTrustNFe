648ce9a2b56187d7a95566a745a43bde3c26eb34
christian-tsmx <christian@tsmx.net.br>
2024-05-27 10:27:20 -0300

    #1114 - 2

--- pytrustnfe/nfse/thema/__init__.py
+++ pytrustnfe/nfse/thema/__init__.py
@@ -5,42 +5,67 @@
 import re
 import os
 import sys
+from pytrustnfe.nfse.thema.assinatura import Assinatura
 from pytrustnfe.xml import render_xml, sanitize_response
 from pytrustnfe.certificado import extract_cert_and_key_from_pfx, save_cert_key
 from lxml import etree
 from requests import Session
 import requests
 
+method2function = {
+    'recepcionarLoteRps': 'NFSEremessa',
+    'consultarLoteRps': 'NFSEconsulta',
+    'consultarNfse': 'NFSEconsulta',
+    'consultarNfsePorRps': 'NFSEconsulta',
+    'consultarSituacaoLoteRps': 'NFSEconsulta',
+}
+
 def _render(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
     parser = etree.XMLParser(
         remove_blank_text=True, remove_comments=True, strip_cdata=False
     )
-    xml_string_send = render_xml(path, "%s.xml" % method, False, **kwargs)
+    signer = Assinatura(certificado.pfx, certificado.password)
+
+    referencia = ""
+    if method == "recepcionarLoteRps":
+        referencia = kwargs.get("nfse").get("numero_lote")
+
+    xml_string_send = render_xml(path, "%s.xml" % method, True, **kwargs)
 
     # xml object
     xml_send = etree.fromstring(
         xml_string_send, parser=parser)
 
-    xml_signed_send = etree.tostring(xml_send)
+    if method == "recepcionarLoteRps":
+        xml_signed_send = signer.assina_xml(
+            xml_send, "lote:{0}".format(referencia))
+    elif method == "cancelarNfse":
+        xml_signed_send = signer.assina_xml(
+            xml_send, kwargs["nfse"]["rps"]["numero"])
+    else:
+        xml_signed_send = etree.tostring(xml_send)
+
+    print ('--- xml ---')
+    print (xml_signed_send)
 
     return xml_signed_send
 
 def _send(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
 
-    url = kwargs["base_url"]
+    url = "%s/%s.%sHttpSoap12Endpoint" %(kwargs["base_url"], method2function[method], method2function[method])
 
     xml_send = kwargs["xml"]
     path = os.path.join(os.path.dirname(__file__), "templates")
-    soap = render_xml(path, "SoapRequest.xml", False, **{"soap_body":xml_send, "method": method })
+    soap = render_xml(path, "SoapRequest.xml", True, **{"soap_body":xml_send, "method": method })
 
     cert, key = extract_cert_and_key_from_pfx(certificado.pfx, certificado.password)
     cert, key = save_cert_key(cert, key)
     session = Session()
     session.cert = (cert, key)
     session.verify = False
-    action = "http://tempuri.org/%s" %(method)
+    action = "urn:%s" %(method)
     headers = {
         "Content-Type": "text/xml;charset=UTF-8",
         "SOAPAction": action,
@@ -48,7 +73,7 @@ def _send(certificado, method, **kwargs):
         "Content-length": str(len(soap))
     }
 
-    request = requests.post(url + '', data=soap, headers=headers)
+    request = requests.post(url, data=soap, headers=headers)
     response, obj = sanitize_response(request.content.decode('utf8', 'ignore'))
     try:
         return {"sent_xml": str(soap), "received_xml": str(response.encode('utf8')), "object": obj.Body }
@@ -56,22 +81,12 @@ def _send(certificado, method, **kwargs):
         return {"sent_xml": str(soap), "received_xml": str(response), "object": obj.Body }
 
 def xml_recepcionar_lote_rps(certificado, **kwargs):
-    return _render(certificado, "RecepcionarLoteRps", **kwargs)
+    return _render(certificado, "recepcionarLoteRps", **kwargs)
 
 def recepcionar_lote_rps(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_recepcionar_lote_rps(certificado, **kwargs)
-    return _send(certificado, "RecepcionarLoteRps", **kwargs)
-
-def xml_recepcionar_lote_rps_sincrono(certificado, **kwargs):
-    return _render(certificado, "RecepcionarLoteRpsSincrono", **kwargs)
-
-def recepcionar_lote_rps_sincrono(certificado, **kwargs):
-    if "xml" not in kwargs:
-        kwargs["xml"] = xml_recepcionar_lote_rps(certificado, **kwargs)
-    if len(kwargs["xml"]["nfse"]["lista_rps"]) > 2:
-        return {}
-    return _send(certificado, "RecepcionarLoteRpsSincrono", **kwargs)
+    return _send(certificado, "recepcionarLoteRps", **kwargs)
 
 def gerar_nfse(certificado, **kwargs):
     return _send(certificado, "GerarNfse", **kwargs)
@@ -113,12 +128,12 @@ def cancelar_nfse(certificado, **kwargs):
     return xml
 
 def xml_consultar_lote_rps(certificado, **kwargs):
-    return _render(certificado, "ConsultarLoteRps", **kwargs)
+    return _render(certificado, "consultarLoteRps", **kwargs)
 
 def consultar_lote_rps(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_consultar_lote_rps(certificado, **kwargs)
-    response = _send(certificado, "ConsultarLoteRps", **kwargs)
+    response = _send(certificado, "consultarLoteRps", **kwargs)
     xml = None
 
     try:
@@ -145,12 +160,12 @@ def consulta_situacao_lote_rps(certificado, **kwargs):
     return _send(certificado, "ConsultaSituacaoLoteRPS", **kwargs)
 
 def xml_consultar_nfse_por_rps(certificado, **kwargs):
-    return _render(certificado, "ConsultarNfsePorRps", **kwargs)
+    return _render(certificado, "consultarNfsePorRps", **kwargs)
 
 def consultar_nfse_por_rps(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_consultar_nfse_por_rps(certificado, **kwargs)
-    response = _send(certificado, "ConsultarNfsePorRps", **kwargs)
+    response = _send(certificado, "consultarNfsePorRps", **kwargs)
     xml = None
 
     try:
