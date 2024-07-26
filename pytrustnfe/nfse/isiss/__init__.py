6f2bb31fadde303aa09aa1e619835d1acfb167ff
Christian David <christian@tsmx.net.br>
2024-07-26 10:40:06 -0300

    #1168 - 3

--- pytrustnfe/nfse/isiss/__init__.py
+++ pytrustnfe/nfse/isiss/__init__.py
@@ -3,19 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import os
-from OpenSSL import crypto
-from base64 import b64encode
-
+import sys
 import requests
-from zeep.transports import Transport
-from requests.packages.urllib3 import disable_warnings
-
 from pytrustnfe.xml import render_xml, sanitize_response
-from pytrustnfe.certificado import extract_cert_and_key_from_pfx, save_cert_key
+from pytrustnfe.certificado import extract_cert_key_and_ca_from_pfx, save_cert_key
 from pytrustnfe.nfe.assinatura import Assinatura
 from lxml import etree
-import sys
-import logging.config
+if sys.version_info[0] > 2:
+    from html.parser import HTMLParser
+else:
+    from HTMLParser import HTMLParser
+
+
 
 def _render(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
@@ -34,18 +33,25 @@ def _render(certificado, method, **kwargs):
     xml_send = etree.fromstring(
         xml_string_send, parser=parser)
 
-    for item in kwargs["nfse"]["lista_rps"]:
-        reference = "rps:{0}{1}".format(
-            item.get('numero'), item.get('serie'))
+    if method in ["RecepcionarLoteRps",
+                  "RecepcionarLoteRpsSincrono"]:
+        for item in kwargs["nfse"]["lista_rps"]:
+            reference = "rps:{0}{1}".format(
+                item.get('numero'), item.get('serie',''))
 
-        signer.assina_xml(xml_send, reference, remove_attrib='Id')
+            signer.assina_xml(xml_send, reference)
 
-    xml_signed_send = signer.assina_xml(
-        xml_send, "lote:{0}".format(referencia))
+        xml_signed_send = signer.assina_xml(
+            xml_send, "lote:{0}".format(referencia))
+    elif method == "CancelarNfse":
+        xml_signed_send = signer.assina_xml(xml_send, "rps:{0}".format(kwargs.get("nfse").get("rps").get("numero")))
+    else:
+        print ('--- xml ---')
+        print (xml_string_send)
+        return xml_string_send
 
     print ('--- xml ---')
     print (xml_signed_send)
-
     return xml_signed_send
 
 def _render_unsigned(certificado, method, **kwargs):
@@ -57,11 +63,6 @@ def _render_unsigned(certificado, method, **kwargs):
     signer = Assinatura(certificado.pfx, certificado.password)
 
     xml = render_xml(path, "%s.xml" % method, True, **kwargs)
-
-    reference = "rps:{0}{1}".format(kwargs["nfse"]['rps']['numero'], 
-                                    kwargs["nfse"]['rps']['serie'])
-    xml_send = etree.fromstring(xml, parser=parser)
-    xml = signer.assina_xml(xml_send, reference, remove_attrib='Id')
 
     return xml
 
@@ -76,39 +77,18 @@ def _send(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
     soap = render_xml(path, "SoapRequest.xml", False, **{"soap_body":xml_send, "method": method })
 
-    cert, key = extract_cert_and_key_from_pfx(certificado.pfx, certificado.password)
-    cert, key = save_cert_key(cert, key)
-    session = requests.Session()
-    session.cert = (cert, key)
     action = 'http://www.abrasf.org.br/nfse.xsd/%s' %(method)
     headers = {
-        "Content-Type": 'text/xml;charset=UTF-8',
-        "SOAPAction": action,
+        'SoapAction': action,
+        'Content-Type': 'text/xml;charset=UTF-8'
     }
-
-    request = session.post(base_url, data=soap, headers=headers)
-    pretty_print_POST(request.request)
-    response, obj = sanitize_response(request.content.decode('utf8', 'ignore'))
-    try:
-        return {"sent_xml": str(soap), "received_xml": str(response.encode('utf8')), "object": obj.Body }
-    except:
-        return {"sent_xml": str(soap), "received_xml": str(response), "object": obj.Body }
-    
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
+    with extract_cert_key_and_ca_from_pfx(certificado.pfx, certificado.password) as cert:
+        request = requests.post(base_url, data=soap, headers=headers, cert=cert)
+        response, obj = sanitize_response(request.content.decode('utf8', 'ignore'))
+        try:
+            return {"sent_xml": str(soap), "received_xml": str(response.encode('utf8')), "object": obj.Body }
+        except:
+            return {"sent_xml": str(soap), "received_xml": str(response), "object": obj.Body }
 
 def xml_recepcionar_lote_rps(certificado, **kwargs):
     return _render(certificado, "RecepcionarLoteRps", **kwargs)
@@ -119,20 +99,50 @@ def recepcionar_lote_rps(certificado, **kwargs):
     return _send(certificado, "RecepcionarLoteRps", **kwargs)
 
 def xml_consultar_lote_rps(certificado, **kwargs):
-    return _render(certificado, "ConsultarLoteRps", **kwargs)
+    return _render_unsigned(certificado, "ConsultarLoteRps", **kwargs)
 
 def consultar_lote_rps(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_consultar_lote_rps(certificado, **kwargs)
-    return _send(certificado, "ConsultarLoteRps", **kwargs)
+    response = _send(certificado, "ConsultarLoteRps", **kwargs)
+    xml = None
+
+    try:
+        xml_element = response['object'].find('.//ConsultarLoteRpsResult')
+
+        if sys.version_info[0] > 2:
+            xml = str(etree.tostring(xml_element, encoding=str))
+        else:
+            xml = str(etree.tostring(xml_element, encoding="utf8"))
+        #unescape
+        xml = HTMLParser().unescape(xml)
+    except:
+        pass
+
+    return xml
 
 def xml_cancelar_nfse(certificado, **kwargs):
-    return _render(certificado, "cancelarNfse", **kwargs)
+    return _render(certificado, "CancelarNfse", **kwargs)
 
 def cancelar_nfse(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_cancelar_nfse(certificado, **kwargs)
-    return _send(certificado, "cancelarNfse", **kwargs)
+    response = _send(certificado, "CancelarNfse", **kwargs)
+    xml = None
+
+    try:
+        xml_element = response['object'].find('.//CancelarNfseResult')
+
+        if sys.version_info[0] > 2:
+            xml = str(etree.tostring(xml_element, encoding=str))
+        else:
+            xml = str(etree.tostring(xml_element, encoding="utf8"))
+        #unescape
+        xml = HTMLParser().unescape(xml)
+    except:
+        pass
+
+    return xml
 
 def consultar_nfse_por_rps(certificado, **kwargs):
     if "xml" not in kwargs:
@@ -141,17 +151,18 @@ def consultar_nfse_por_rps(certificado, **kwargs):
     print (kwargs["xml"])
     
     response = _send(certificado, "ConsultarNfsePorRps", **kwargs)
+    print(response['received_xml'])
     xml = None
 
     try:
-        xml_element = response['object'].find('.//Nfse')
+        xml_element = response['object'].find('.//ConsultarNfsePorRpsResult')
 
         if sys.version_info[0] > 2:
             xml = str(etree.tostring(xml_element, encoding=str))
         else:
             xml = str(etree.tostring(xml_element, encoding="utf8"))
-            
-        xml = xml.replace('&#13;', '')
+        #unescape
+        xml = HTMLParser().unescape(xml)
     except:
         pass
 
