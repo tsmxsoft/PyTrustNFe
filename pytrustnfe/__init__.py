e9fd742af61992355aa7c9222b7a6b4f17b55403
christian-tsmx <137795700+christian-tsmx@users.noreply.github.com>
2024-09-30 13:29:42 -0300

    version up

--- pytrustnfe/__init__.py
+++ pytrustnfe/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 def get_version():
-    return "1.0.260"
+    return "1.0.261"
 
 class HttpClient(object):
     def __init__(self, url):
