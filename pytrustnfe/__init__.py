41a87212b3a268ed795d289c4feafc88ad9d8f20
Junior Andrade <seisxis@gmail.com>
2024-09-30 13:00:48 -0300

    issnet intermediador

--- pytrustnfe/__init__.py
+++ pytrustnfe/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 def get_version():
-    return "1.0.258"
+    return "1.0.259"
 
 class HttpClient(object):
     def __init__(self, url):
