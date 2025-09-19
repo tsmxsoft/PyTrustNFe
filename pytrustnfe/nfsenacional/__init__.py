ad31d40a552fa1f1413724979f40540f84c48ee7
RodrigoVictor01 <rodrigovictor@MacBook-Pro-de-Rodrigo.local>
2025-09-19 14:31:31 -0300

    Ajustes NFSe Nacional

--- pytrustnfe/nfsenacional/__init__.py
+++ pytrustnfe/nfsenacional/__init__.py
@@ -95,7 +95,9 @@ def _render(certificado, method, sign, **kwargs):
     )
     signer = Assinatura(certificado.pfx, certificado.password)
     xml_string_send = render_xml(path, "%s_%s.xml" % (method, VERSAO), True, **kwargs)
-    
+
+    print('xml_send', xml_string_send)
+
     xmlElem_send = etree.fromstring(
         xml_string_send, parser=parser)
 
@@ -456,6 +458,8 @@ def _abrasf_adapter(**kwargs):
             }
         }
     }
+    print('*------NFSE BASE------*', nfse_base)
+
     if op_simples in ['1','2']:
         nfse_base["infDPS"]["prest"]["regTrib"].pop("regApTribSN")
     #Exigibilidade ISS
